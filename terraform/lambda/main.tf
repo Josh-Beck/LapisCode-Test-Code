e80d60998856432335ff5b2e48fa1da75acc32dd
Joshua Beck <57635860+Josh-Beck@users.noreply.github.com>
2023-02-10 08:57:29 -0500

    Change variables to strings for parsing error

--- terraform/lambda/main.tf
+++ terraform/lambda/main.tf
@@ -43,8 +43,8 @@ EOF
 }
  
 resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
- role        = aws_iam_role.lambda_role.name
- policy_arn  = aws_iam_policy.iam_policy_for_lambda.arn
+ role        = "aws_iam_role.lambda_role.name"
+ policy_arn  = "aws_iam_policy.iam_policy_for_lambda.arn"
 }
  
 data "archive_file" "zip_the_python_code" {
@@ -56,7 +56,7 @@ output_path = "${path.module}/python/hello-python.zip"
 resource "aws_lambda_function" "terraform_lambda_func" {
 filename                       = "${path.module}/python/hello-python.zip"
 function_name                  = "Spacelift_Test_Lambda_Function"
-role                           = aws_iam_role.lambda_role.arn
+role                           = "aws_iam_role.lambda_role.arn"
 handler                        = "index.lambda_handler"
 runtime                        = "python3.8"
 depends_on                     = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
