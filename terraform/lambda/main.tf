5e81118af06adf89da8b89922c1e65a8eaa742cc
Joshua Beck <57635860+Josh-Beck@users.noreply.github.com>
2023-02-12 14:36:11 -0500

    Update main.tf

--- terraform/lambda/main.tf
+++ terraform/lambda/main.tf
@@ -55,7 +55,7 @@ output_path = "${path.module}/python/hello-python.zip"
  
 resource "aws_lambda_function" "terraform_lambda_func" {
 filename                       = "${path.module}/python/hello-python.zip"
-function_name                  = "Spacelift_Test_Lambda_Function"
+function_name                  = "Test_Lambda_Function"
 role                           = "aws_iam_role.lambda_role.arn"
 handler                        = "index.lambda_handler"
 runtime                        = "python3.8"
