8cb94e629b9d6bd478f1cefd97dd9862c325f236
Joshua Beck <57635860+Josh-Beck@users.noreply.github.com>
2023-02-13 21:30:42 -0500

    Update index.py

--- python/index.py
+++ python/index.py
@@ -1,5 +1,11 @@
 def lambda_handler(event, context):
    message = 'Hello {} !'.format(event['key1'])
+   
+   #Test parameters
+   body = event.get("body")
+   queryParams = event["queryStringParameters"]
+   auth = event["headers"]["authorization"]
+   
    return {
        'message' : message
    }
