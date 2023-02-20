9ba2bc87e8ba9896620ba2c9ca2243f00d99f796
Joshua Beck <57635860+Josh-Beck@users.noreply.github.com>
2023-02-19 22:05:48 -0500

    Update handler.js

--- js/handler.js
+++ js/handler.js
@@ -1,6 +1,31 @@
+"use strict";
 
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3();
 
-exports.handler =  async function(event, context) {
-  console.log("EVENT: \n" + JSON.stringify(event, null, 2))
-  return "Test";
+module.exports.handler = (event, context, callback) => {
+    let requestBody = JSON.parse(event.body);
+    let url = requestBody.url;
+    let objectId = event.queryStringParameters.objectId;
+    let objectKey = `${objectId}.png`;
+    let bucket = event.headers.bucketName;
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => uploadToS3(data, objectKey, bucket));
+};
+
+/**
+* @param {*} data
+* @param {string} key
+*/
+function uploadToS3(data, key, bucket) {
+    return s3
+        .putObject({
+            Bucket: bucket,
+            Key: key,
+            Body: data,
+            ContentType: "application/json"
+        })
+        .promise();
 }
